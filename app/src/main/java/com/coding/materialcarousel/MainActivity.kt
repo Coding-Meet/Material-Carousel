751e4209071a7a7deb905bba69807d56001a193e
coding meet <codingmeet26@gmail.com>
2023-10-19 09:47:16 +0530

    Initial commit

---
+++ app/src/main/java/com/coding/materialcarousel/MainActivity.kt
@@ -0,0 +1,50 @@
+ package com.coding.materialcarousel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.coding.materialcarousel.adapters.ImageAdapter
+import com.coding.materialcarousel.models.ImageItem
+import java.util.UUID
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageRV = findViewById<RecyclerView>(R.id.imageRV)
+
+
+        val imageList = arrayListOf(
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/866/500/500.jpg?hmac=FOptChXpmOmfR5SpiL2pp74Yadf1T_bRhBF1wJZa9hg"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/270/500/500.jpg?hmac=MK7XNrBrZ73QsthvGaAkiNoTl65ZDlUhEO-6fnd-ZnY"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/320/500/500.jpg?hmac=2iE7TIF9kIqQOHrIUPOJx2wP1CJewQIZBeMLIRrm74s"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/798/500/500.jpg?hmac=Bmzk6g3m8sUiEVHfJWBscr2DUg8Vd2QhN7igHBXLLfo"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/95/500/500.jpg?hmac=0aldBQ7cQN5D_qyamlSP5j51o-Og4gRxSq4AYvnKk2U"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/778/500/500.jpg?hmac=jZLZ6WV_OGRxAIIYPk7vGRabcAGAILzxVxhqSH9uLas"
+            )
+        )
+
+        val imageAdapter = ImageAdapter()
+        imageRV.adapter = imageAdapter
+        imageAdapter.submitList(imageList)
+
+    }
+}
---
+++ app/src/main/java/com/coding/materialcarousel/models/ImageItem.kt
@@ -0,0 +1,6 @@
+package com.coding.materialcarousel.models
+
+data class ImageItem(
+    val id : String,
+    val url : String
+)
